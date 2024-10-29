9ccf5911cbc200793b64a96862d3d04941127e29
Aurélien <atondoux@gmail.com>
2024-10-29 21:47:00 +0100

    :sparkles: add player identifier

---
+++ src/main/resources/db/migrations/prod/V006__update_players_with_identifier.sql
@@ -0,0 +1,10 @@
+UPDATE player SET identifier='novakdjokovic' WHERE last_name = 'Djokovic';
+UPDATE player SET identifier='carlosalcaraz' WHERE last_name = 'Alcaraz';
+UPDATE player SET identifier='janniksinner' WHERE last_name = 'Sinner';
+UPDATE player SET identifier='daniilmedvedev' WHERE last_name = 'Medvedev';
+UPDATE player SET identifier='andreyrublev' WHERE last_name = 'Rublev';
+UPDATE player SET identifier='alexanderzverev' WHERE last_name = 'Zverev';
+UPDATE player SET identifier='holgerrune' WHERE last_name = 'Rune';
+UPDATE player SET identifier='huberthurkacz' WHERE last_name = 'Hurkacz';
+UPDATE player SET identifier='alexdeminaur' WHERE last_name = 'De Minaur';
+UPDATE player SET identifier='taylorfritz' WHERE last_name = 'Fritz';
---
+++ src/test/resources/db/migrations/test/V006__update_players_with_identifier.sql
@@ -0,0 +1,4 @@
+UPDATE player SET identifier='rafaelnadaltest' WHERE last_name = 'NadalTest';
+UPDATE player SET identifier='novakdjokovictest' WHERE last_name = 'DjokovicTest';
+UPDATE player SET identifier='rogerfederertest'	WHERE last_name = 'FedererTest';
+UPDATE player SET identifier='andymurraytest' WHERE last_name = 'MurrayTest';
